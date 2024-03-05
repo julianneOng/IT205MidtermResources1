dc8d11b03e54d794765a193fdf48c402e41bd0df
julianneOng <101238387+julianneOng@users.noreply.github.com>
2024-03-05 15:41:27 +0800

    First commit

---
+++ input.py
@@ -0,0 +1,10 @@
+#input() has string data type
+num1 = int(input("Enter 1st number: "))
+int(num1)
+#int() for converting string to integer
+num2 = int(input("Enter 2nd number: "))
+int(num2)
+
+sum = num1 + num2
+
+print("The sum of {num1} and {num2} is: {sum}")
---
+++ output.py
@@ -0,0 +1,12 @@
+#variable declaration
+
+x = 25
+message = "Hi po!"
+ratio = 3.14
+#checking the type of data with type() method.
+print(type(message))
+print(type(ratio))
+print(type(x))
+#string formatting- f for fstring
+print(message + "my name is jessa. I am "+str (x) +"years old")
+printf(f"{message} my name is jessa. I am {x} years old.")
